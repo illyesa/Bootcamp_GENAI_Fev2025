4bba03df54ba1f9cd078ed3450f0cd535dbbfe84
illyesa <illyes.akabi@gmail.com>
2025-02-06 14:20:16 +0100

    je suis a lexo 4

--- Crashcourse/Introduction to Python Programming/exo.py
+++ Crashcourse/Introduction to Python Programming/exo.py
@@ -1,3 +1,24 @@
 
 #1
-print("Hellow, -g, world")
+print(("\033[32mHello\033[0m world\n" * 4).strip())
+
+#2
+print((99**3)*8)
+
+#3
+your_name=input("Your name :")
+my_name="my_name"
+if your_name == my_name:
+    print(f"we have the same {my_name}")
+else:
+    print("diff")
+
+
+#4
+tall=int(input("Your height in centimeters :"))
+if tall>145:
+    print("tall enough to ride")
+else:
+    print("too small")
+
+#5
