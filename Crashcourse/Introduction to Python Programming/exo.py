f643c11852a6d7463d6e4abde853b73c26ad03cc
illyesa <illyes.akabi@gmail.com>
2025-02-07 09:43:23 +0100

    exo

--- Crashcourse/Introduction to Python Programming/exo.py
+++ Crashcourse/Introduction to Python Programming/exo.py
@@ -1,53 +1,53 @@
 
-# #1
-# print(("\033[32mHello\033[0m world\n" * 4).strip())
+#1
+print(("\033[32mHello\033[0m world\n" * 4).strip())
 
-# #2
-# print((99**3)*8)
+#2
+print((99**3)*8)
 
-# #3
-# your_name=input("Your name :")
-# my_name="my_name"
-# if your_name == my_name:
-#     print(f"we have the same {my_name}")
-# else:
-#     print("diff")
+#3
+your_name=input("Your name :")
+my_name="my_name"
+if your_name == my_name:
+    print(f"we have the same {my_name}")
+else:
+    print("diff")
 
 
-# #4
-# tall=int(input("Your height in centimeters :"))
-# if tall>145:
-#     print("tall enough to ride")
-# else:
-#     print("too small")
+#4
+tall=int(input("Your height in centimeters :"))
+if tall>145:
+    print("tall enough to ride")
+else:
+    print("too small")
 
-# #5
-# my_fav_numbers = {2, 10, 20}
-# my_fav_numbers.add(26)
-# my_fav_numbers.add(42)
-# print(f"My favorite numbers after adding: {my_fav_numbers}")
+#5
+my_fav_numbers = {2, 10, 20}
+my_fav_numbers.add(26)
+my_fav_numbers.add(42)
+print(f"My favorite numbers after adding: {my_fav_numbers}")
 
-# my_fav_numbers.remove(42)
-# print(f"My favorite numbers after removing the last added number: {my_fav_numbers}")
-# friend_fav_numbers = {3, 9, 27}
-# our_fav_numbers = my_fav_numbers.union(friend_fav_numbers)
-# print(f"Our favorite numbers combined: {our_fav_numbers}")
+my_fav_numbers.remove(42)
+print(f"My favorite numbers after removing the last added number: {my_fav_numbers}")
+friend_fav_numbers = {3, 9, 27}
+our_fav_numbers = my_fav_numbers.union(friend_fav_numbers)
+print(f"Our favorite numbers combined: {our_fav_numbers}")
 
 #6
-# my_tuple = (1, 2, 3) # on ne peut pas ajouter a un tuple mais on peut en crée un nouveau
-# new_tuple = my_tuple + (4, 5)
-# print(new_tuple)
+my_tuple = (1, 2, 3) # on ne peut pas ajouter a un tuple mais on peut en crée un nouveau
+new_tuple = my_tuple + (4, 5)
+print(new_tuple)
 
 #7
-# basket = ["Banana", "Apples", "Oranges", "Blueberries"]
-# basket.remove("Banana")
-# basket.remove("Blueberries")
-# basket.append("kiwi")
-# basket.insert(0, "Apples")
-# print(basket)
-# print(basket.count("Apples"))
-# basket.clear()
-# print(basket)
+basket = ["Banana", "Apples", "Oranges", "Blueberries"]
+basket.remove("Banana")
+basket.remove("Blueberries")
+basket.append("kiwi")
+basket.insert(0, "Apples")
+print(basket)
+print(basket.count("Apples"))
+basket.clear()
+print(basket)
 
 #8
 sandwich_orders = ["Tuna sandwich", "Pastrami sandwich", "Avocado sandwich", "Pastrami sandwich", "Egg sandwich", "Chicken sandwich", "Pastrami sandwich"]
