2d36a67e64292928eb7751064ce55431841e3c04
illyesa <illyes.akabi@gmail.com>
2025-02-12 11:38:03 +0100

    avancement 2 day4

--- Week1/Day4/ExercicesXP.py
+++ Week1/Day4/ExercicesXP.py
@@ -91,9 +91,36 @@
 #             print("not trained")
 
 #4
-class Familly():
-    def __init__(self, members, last_name):
+class Family():
+    def __init__(self, last_name, members):
         self.members = members
         self.last_name = last_name
-        
+    def born(self, **kwargs):
+        self.members.append(kwargs)
+        print("Congratulation")
+    def is_18(self, name):
+        for i in range(len(self.members)):
+            if self.members[i]["name"]==name:
+        #if self.members["name"] == name:
+                return self.members[i]["age"] >= 18
+            else:
+                return False
+        else:
+            print(f"{name} is not in this family")
+    def family_presentation(self):
+        print(f"Family name: {self.last_name}")
+        for member in self.members:
+            #member.append("is_child", self.is_18(member["age"]))
+            print(member)
+
+family = Family("Smith", [
+    {"name": "Michael", "age": 35, "gender": "Male", "is_child": False},
+    {"name": "Sarah", "age": 32, "gender": "Female", "is_child": False}
+])
+family.family_presentation()
+family.born(name='John', age=0, gender='Male', is_child=True)
+print(family.members[0]["name"])
+print(family.is_18("Michael"))
+family.family_presentation()
+
 
